b55bcdd6a7f4734e6d4269761fca9e5d2ebfe3ed
Oksana Shevchenko <oksana-shevchenko@ukr.net>
2020-07-02 04:02:41 +0300

    Change prebuild kernel path
    Signed-off-by: Oksana Shevchenko <oksana-shevchenko@ukr.net>

--- hikey960.mk
+++ hikey960.mk
@@ -1,8 +1,10 @@
 ifndef TARGET_KERNEL_USE
-TARGET_KERNEL_USE=4.9
+	TARGET_KERNEL_USE=4.9
+else
+	$(error only default 4.9 is supported)
 endif
-TARGET_PREBUILT_KERNEL := device/linaro/hikey-kernel/Image.gz-hikey960-$(TARGET_KERNEL_USE)
-TARGET_PREBUILT_DTB := device/linaro/hikey-kernel/hi3660-hikey960.dtb-$(TARGET_KERNEL_USE)
+TARGET_PREBUILT_KERNEL := out/target/product/hikey960/Image.gz-hikey960-4.9
+TARGET_PREBUILT_DTB := out/target/product/hikey960/hi3660-hikey960.dtb-4.9
 
 ifeq ($(TARGET_KERNEL_USE), 4.4)
   HIKEY_USE_LEGACY_TI_BLUETOOTH := true
@@ -16,7 +18,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, device/linaro/hikey/hikey960/device-hikey960.mk)
 $(call inherit-product, device/linaro/hikey/device-common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-
+$(call inherit-product, vendor/gl/hikey960/device-vendor.mk)
 #
 # Overrides
 PRODUCT_NAME := hikey960
